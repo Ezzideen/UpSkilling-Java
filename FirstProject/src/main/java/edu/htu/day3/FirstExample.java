4ca1cf69d7851e4d8d8adc80dd88e14d77eb3b3c
MohammadAbuJajeh <mbaj2888@gmail.com>
2020-11-05 09:19:30 +0200

    FirstExample

---
+++ FirstProject/src/main/java/edu/htu/day3/FirstExample.java
@@ -0,0 +1,24 @@
+package edu.htu.day3;
+
+public class FirstExample {
+
+	public static void main(String[] args) {
+		byte b = 1;
+		short s = 2;
+		char c = 'A';
+		int i = 15;
+		long l = 5;
+		float f = 2.2f;
+		double d = 5.5;
+		
+		boolean t = true;
+		boolean fa = false;
+		
+		int ii;
+		ii = 10;
+		
+		System.out.println(i);
+		
+	}
+
+}
