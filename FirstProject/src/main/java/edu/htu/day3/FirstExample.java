b5868ae46d6dc51f5d8fc4194cca14aac652ad4d
alibanyali <banyali@bk.ru>
2020-11-06 18:57:13 +0200

    AliBanyAli test

--- FirstProject/src/main/java/edu/htu/day3/FirstExample.java
+++ FirstProject/src/main/java/edu/htu/day3/FirstExample.java
@@ -2,23 +2,32 @@ package edu.htu.day3;
 
 public class FirstExample {
 
+
 	public static void main(String[] args) {
-		byte b = 1;
-		short s = 2;
-		char c = 'A';
-		int i = 15;
-		long l = 5;
-		float f = 2.2f;
-		double d = 5.5;
-		
-		boolean t = true;
-		boolean fa = false;
-		
-		int ii;
-		ii = 10;
-		
-		System.out.println(i);
-		
+		String s1 = "5";
+		String s2 = "6";
+		System.out.println(s1 + s2);
+		System.out.println(s1 + 5);
+		System.out.println(1 + "" + 5);
+		System.out.println(1 + 5);
+
+		String s3 = s1.concat(s2);
+		System.out.println(s3);
+
+		int[] a = new int[5];
+		int[] b = { 1, 2, 3, 4, 5 };
+
+		a[3] = 15;
+
+		System.out.println(b.length);
+		System.out.println(a[3]);
+		System.out.println("alibanyali");
+		double d= Double.parseDouble(s3);
+		double dd= Double.valueOf(s3);
+		System.out.println(d);
+	    System.out.println(dd);
+	    
+
 	}
 
 }
