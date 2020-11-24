8dbdd48964bfbd60c995dc666ce2c2e6cb732e41
SarahMalash <sarahabumalash@gmail.com>
2020-11-25 00:21:20 +0200

    sarah Malash

---
+++ FirstProject/src/main/java/assinment1/SarahMalash/src/main/java/assi1/HomeWork.java
@@ -0,0 +1,26 @@
+package assi1;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
+
+public class HomeWork {
+
+	public static void main(String[] args) throws IOException {
+Helper help = new Helper();
+
+		help.CreateBuuton();
+		
+	}
+
+	
+}
+
